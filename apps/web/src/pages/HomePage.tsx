8258b01bc9e4d44179a7645c3a456e34d49143ca
Julian Leahy <julian.leahy.dev@gmail.com>
2026-02-19 12:17:04 +1300

    Add temp navigation button to HomePage for redirecting to All Clients

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
 import Layout from '../components/Layout'
+import { useNavigate } from 'react-router-dom'
 
 export default function HomePage() {
+  const navigate = useNavigate()
   return (
     <Layout>
       <div>Knowledge Base Card</div>
       <div>Client Briefs Card</div>
+      <button
+        className="mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+        onClick={() => navigate('/')}
+      >
+        Go to All Clients
+      </button>
     </Layout>
   )
 }
